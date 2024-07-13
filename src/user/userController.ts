b1500e0002db649ad2a225a145021810a1d3586d
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-07-13 16:58:05 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -14,25 +15,41 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exist wiht this email.");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(400, "User already exist wiht this email.");
+      return next(error);
+    }
+  } catch (e) {
+    return next(createHttpError(500, "Error while getting user"));
   }
 
   // Password Hashing Logic
   const hashedPassword = await bcrypt.hash(password, 10);
   // store the user in db using create menthod in moggoose!
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  //  token generation JWT
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
+
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return next(createHttpError(500, "Error while while creating user"));
+  }
+  let token;
+  try {
+    //  token generation JWT
+    token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+  } catch (err) {
+    return next(createHttpError(500, "Error while signing the jwt token"));
+  }
+
   // response
   res.json({ accessToken: token });
 };
