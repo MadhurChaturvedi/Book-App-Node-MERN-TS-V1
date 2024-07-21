87c09e70cc04c2c2be22e579ff08435efacc5375
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-07-21 20:32:22 +0530

    create book

--- src/book/bookContoller.ts
+++ src/book/bookContoller.ts
@@ -2,8 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
+import fs from "node:fs";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
   const files = req.files as { [filename: string]: Express.Multer.File[] };
   const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
 
@@ -40,9 +43,17 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     console.log("Upload result", uploadResult);
     console.log("Upload result", bookFileUploadResult);
 
-    res.status(200).json({
-      message: "ok",
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "6692c6865d16866624f4fab6",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
     });
+
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+    res.status(201).json({ id: newBook._id });
   } catch (err) {
     return next(createHttpError(500, "Error"));
   }
